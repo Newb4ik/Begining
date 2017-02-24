3367c113a05a8f1f68f6e7fc022f7de10134739b
Newb4ik <Newb4ik@gmail.com>
2017-02-24 13:14:04 +0500

    Каркас
    NewProject

---
+++ index.php
@@ -0,0 +1,46 @@
+
+<!DOCTYPE html>
+<!--[if lt IE 7]><html lang="ru" class="lt-ie9 lt-ie8 lt-ie7"><![endif]-->
+<!--[if IE 7]><html lang="ru" class="lt-ie9 lt-ie8"><![endif]-->
+<!--[if IE 8]><html lang="ru" class="lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!-->
+<html lang="ru">
+<!--<![endif]-->
+<head>
+	<meta charset="utf-8" />
+	<title>ShubinTV</title>
+	<meta name="description" content="" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="shortcut icon" href="favicon.png" />
+	<link rel="stylesheet" href="css/fonts.css" />
+	<link rel="stylesheet" href="css/main.css" />
+	<link rel="stylesheet" href="css/media.css" />
+</head>
+<body>
+
+	<!--[if lt IE 9]>
+	<script src="libs/html5shiv/es5-shim.min.js"></script>
+	<script src="libs/html5shiv/html5shiv.min.js"></script>
+	<script src="libs/html5shiv/html5shiv-printshiv.min.js"></script>
+	<script src="libs/respond/respond.min.js"></script>
+	<![endif]-->
+	
+	<script script src="libs/jquery/jquery-1.11.1.min.js"></script>
+	<script src="libs\html5shiv\html5shiv.min.js" type="text/javascript"></script>
+	<script src="js/common.js"></script>
+	<!-- Yandex.Metrika counter --><!-- /Yandex.Metrika counter -->
+	<!--  Google Analytics counter --><!-- /Google Analytics counter -->
+
+	<header>
+		<h1>Юрист В Онлайн</h1>	
+		<p>Помоги себе сам!</p>
+	</header>
+	<nav>
+		<ul>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		</ul>
+	</nav>
